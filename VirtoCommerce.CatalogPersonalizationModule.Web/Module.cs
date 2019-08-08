a0a1785fe3fa66516bfa140f6d8ecc694fc9806a
Andrey Kozlov <yecli.github@gmail.com>
2019-08-08 18:01:14 +0500

    #16: Added UserGroups handling for categories using CategorySearchUserGroupsRequestBuilder.

--- VirtoCommerce.CatalogPersonalizationModule.Web/Module.cs
+++ VirtoCommerce.CatalogPersonalizationModule.Web/Module.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Practices.Unity;
 using VirtoCommerce.CatalogModule.Data.Search;
 using VirtoCommerce.CatalogPersonalizationModule.Core.Services;
 using VirtoCommerce.CatalogPersonalizationModule.Data.Model;
@@ -20,111 +20,112 @@ using VirtoCommerce.Platform.Data.Repositories;
 
 namespace VirtoCommerce.CatalogPersonalizationModule.Web
 {
-    public class Module : ModuleBase, ISupportExportImportModule
-    {
-        private const string _connectionStringName = "VirtoCommerce";
-        private readonly IUnityContainer _container;
+	public class Module : ModuleBase, ISupportExportImportModule
+	{
+		private const string _connectionStringName = "VirtoCommerce";
+		private readonly IUnityContainer _container;
 
-        public Module(IUnityContainer container)
-        {
-            _container = container;
-        }
+		public Module(IUnityContainer container)
+		{
+			_container = container;
+		}
 
-        #region IModule Members
+		#region IModule Members
 
-        public override void SetupDatabase()
-        {
-            using (var context = new PersonalizationRepositoryImpl(_connectionStringName, _container.Resolve<AuditableInterceptor>()))
-            {
-                var initializer = new SetupDatabaseInitializer<PersonalizationRepositoryImpl, Data.Migrations.Configuration>();
-                initializer.InitializeDatabase(context);
-            }
-        }
+		public override void SetupDatabase()
+		{
+			using (var context = new PersonalizationRepositoryImpl(_connectionStringName, _container.Resolve<AuditableInterceptor>()))
+			{
+				var initializer = new SetupDatabaseInitializer<PersonalizationRepositoryImpl, Data.Migrations.Configuration>();
+				initializer.InitializeDatabase(context);
+			}
+		}
 
-        public override void Initialize()
-        {
-            base.Initialize();
+		public override void Initialize()
+		{
+			base.Initialize();
 
-            _container.RegisterType<IPersonalizationRepository>(new InjectionFactory(c => new PersonalizationRepositoryImpl(_connectionStringName, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>(),
-                new ChangeLogInterceptor(_container.Resolve<Func<IPlatformRepository>>(), ChangeLogPolicy.Cumulative, new[] { typeof(TaggedItemEntity).Name }))));
-            _container.RegisterType<ITaggedItemService, PersonalizationService>();
-            _container.RegisterType<ITaggedItemSearchService, PersonalizationService>();
+			_container.RegisterType<IPersonalizationRepository>(new InjectionFactory(c => new PersonalizationRepositoryImpl(_connectionStringName, new EntityPrimaryKeyGeneratorInterceptor(), _container.Resolve<AuditableInterceptor>(),
+				new ChangeLogInterceptor(_container.Resolve<Func<IPlatformRepository>>(), ChangeLogPolicy.Cumulative, new[] { typeof(TaggedItemEntity).Name }))));
+			_container.RegisterType<ITaggedItemService, PersonalizationService>();
+			_container.RegisterType<ITaggedItemSearchService, PersonalizationService>();
 
-            //_container.RegisterType<ProductSearchUserGroupsRequestBuilder>();
-            _container.RegisterType<ISearchRequestBuilder, ProductSearchUserGroupsRequestBuilder>(nameof(ProductSearchRequestBuilder));
-        }
+			//_container.RegisterType<ProductSearchUserGroupsRequestBuilder>();
+			_container.RegisterType<ISearchRequestBuilder, ProductSearchUserGroupsRequestBuilder>(nameof(ProductSearchRequestBuilder));
+			_container.RegisterType<ISearchRequestBuilder, CategorySearchUserGroupsRequestBuilder>(nameof(CategorySearchRequestBuilder));
+		}
 
-        public override void PostInitialize()
-        {
-            base.PostInitialize();
+		public override void PostInitialize()
+		{
+			base.PostInitialize();
 
-            #region Search
+			#region Search
 
-            // Add tagged items document source to the category or product indexing configuration
-            var documentIndexingConfigurations = _container.Resolve<IndexDocumentConfiguration[]>();
-            if (documentIndexingConfigurations != null)
-            {
-                //Category indexing
-                var taggedItemCategoryDocumentSource = new IndexDocumentSource
-                {
-                    ChangesProvider = _container.Resolve<TaggedItemIndexChangesProvider>(),
-                    DocumentBuilder = _container.Resolve<TaggedItemCategoryDocumentBuilder>(),
-                };
-                foreach (var configuration in documentIndexingConfigurations.Where(c => c.DocumentType == KnownDocumentTypes.Category))
-                {
-                    if (configuration.RelatedSources == null)
-                    {
-                        configuration.RelatedSources = new List<IndexDocumentSource>();
-                    }
+			// Add tagged items document source to the category or product indexing configuration
+			var documentIndexingConfigurations = _container.Resolve<IndexDocumentConfiguration[]>();
+			if (documentIndexingConfigurations != null)
+			{
+				//Category indexing
+				var taggedItemCategoryDocumentSource = new IndexDocumentSource
+				{
+					ChangesProvider = _container.Resolve<TaggedItemIndexChangesProvider>(),
+					DocumentBuilder = _container.Resolve<TaggedItemCategoryDocumentBuilder>(),
+				};
+				foreach (var configuration in documentIndexingConfigurations.Where(c => c.DocumentType == KnownDocumentTypes.Category))
+				{
+					if (configuration.RelatedSources == null)
+					{
+						configuration.RelatedSources = new List<IndexDocumentSource>();
+					}
 
-                    configuration.RelatedSources.Add(taggedItemCategoryDocumentSource);
-                }
+					configuration.RelatedSources.Add(taggedItemCategoryDocumentSource);
+				}
 
-                //Product indexing
-                var taggedItemProductDocumentSource = new IndexDocumentSource
-                {
-                    ChangesProvider = _container.Resolve<TaggedItemIndexChangesProvider>(),
-                    DocumentBuilder = _container.Resolve<TaggedItemProductDocumentBuilder>(),
-                };
-                foreach (var configuration in documentIndexingConfigurations.Where(c => c.DocumentType == KnownDocumentTypes.Product))
-                {
-                    if (configuration.RelatedSources == null)
-                    {
-                        configuration.RelatedSources = new List<IndexDocumentSource>();
-                    }
+				//Product indexing
+				var taggedItemProductDocumentSource = new IndexDocumentSource
+				{
+					ChangesProvider = _container.Resolve<TaggedItemIndexChangesProvider>(),
+					DocumentBuilder = _container.Resolve<TaggedItemProductDocumentBuilder>(),
+				};
+				foreach (var configuration in documentIndexingConfigurations.Where(c => c.DocumentType == KnownDocumentTypes.Product))
+				{
+					if (configuration.RelatedSources == null)
+					{
+						configuration.RelatedSources = new List<IndexDocumentSource>();
+					}
 
-                    configuration.RelatedSources.Add(taggedItemProductDocumentSource);
-                }
-            }
+					configuration.RelatedSources.Add(taggedItemProductDocumentSource);
+				}
+			}
 
-            #endregion
-        }
+			#endregion
+		}
 
-        #endregion
+		#endregion
 
-        #region ISupportExportImportModule Members
+		#region ISupportExportImportModule Members
 
-        public void DoExport(System.IO.Stream outStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
-        {
-            var exportJob = _container.Resolve<PersonalizationExportImport>();
-            exportJob.DoExport(outStream, progressCallback);
-        }
+		public void DoExport(System.IO.Stream outStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+		{
+			var exportJob = _container.Resolve<PersonalizationExportImport>();
+			exportJob.DoExport(outStream, progressCallback);
+		}
 
-        public void DoImport(System.IO.Stream inputStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
-        {
-            var exportJob = _container.Resolve<PersonalizationExportImport>();
-            exportJob.DoImport(inputStream, progressCallback);
-        }
+		public void DoImport(System.IO.Stream inputStream, PlatformExportManifest manifest, Action<ExportImportProgressInfo> progressCallback)
+		{
+			var exportJob = _container.Resolve<PersonalizationExportImport>();
+			exportJob.DoImport(inputStream, progressCallback);
+		}
 
-        public string ExportDescription
-        {
-            get
-            {
-                var settingManager = _container.Resolve<ISettingsManager>();
-                return settingManager.GetValue("Personalization.ExportImport.Description", string.Empty);
-            }
-        }
+		public string ExportDescription
+		{
+			get
+			{
+				var settingManager = _container.Resolve<ISettingsManager>();
+				return settingManager.GetValue("Personalization.ExportImport.Description", string.Empty);
+			}
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
