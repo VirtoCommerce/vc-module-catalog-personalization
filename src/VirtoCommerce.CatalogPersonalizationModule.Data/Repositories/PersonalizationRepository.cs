2a11df0081bbb53dfc3b226403df1b9507870f22
alexandr andreev <alivemenone@gmail.com>
2020-03-26 09:41:31 +0300

    VP-1557: Fix error when synchronize outline for UpTree propagation (#30)
    Using actual Microsoft.Data.SqlClient instead of the old System.Data.SqlClient

--- src/VirtoCommerce.CatalogPersonalizationModule.Data/Repositories/PersonalizationRepository.cs
+++ src/VirtoCommerce.CatalogPersonalizationModule.Data/Repositories/PersonalizationRepository.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using VirtoCommerce.CatalogPersonalizationModule.Core.Model.Search;
 using VirtoCommerce.CatalogPersonalizationModule.Data.Model;
