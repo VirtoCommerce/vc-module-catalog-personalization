f46d34d2d4deb7c2eed7b6981e78be84ca564d05
Aleksandr Andreev <alivemenone@gmail.com>
2020-06-01 08:56:03 +0300

    VP-2291: Rewrite export according to V3 pattern Fix broken export

--- src/VirtoCommerce.CatalogPersonalizationModule.Web/ExportImport/PersonalizationExportImport.cs
+++ src/VirtoCommerce.CatalogPersonalizationModule.Web/ExportImport/PersonalizationExportImport.cs
@@ -1,60 +1,94 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using VirtoCommerce.CatalogPersonalizationModule.Core.Model;
 using VirtoCommerce.CatalogPersonalizationModule.Core.Model.Search;
 using VirtoCommerce.CatalogPersonalizationModule.Core.Services;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.ExportImport;
+using VirtoCommerce.Platform.Data.ExportImport;
 
 namespace VirtoCommerce.CatalogPersonalizationModule.Web.ExportImport
 {
-    public sealed class BackupObject
-    {
-        public ICollection<TaggedItem> TaggedItems { get; set; }
-    }
-
     public sealed class PersonalizationExportImport
     {
         private readonly ITaggedItemService _taggedItemService;
         private readonly ITaggedItemSearchService _taggedItemSearchService;
+        private readonly JsonSerializer _serializer;
+        private readonly int _batchSize = 50;
 
-        public PersonalizationExportImport(ITaggedItemService taggedItemService, ITaggedItemSearchService taggedItemSearchService)
+        public PersonalizationExportImport(ITaggedItemService taggedItemService, ITaggedItemSearchService taggedItemSearchService, JsonSerializer jsonSerializer)
         {
             _taggedItemService = taggedItemService;
             _taggedItemSearchService = taggedItemSearchService;
+            _serializer = jsonSerializer;
         }
 
-        public async Task DoExportAsync(Stream backupStream, Action<ExportImportProgressInfo> progressCallback)
+        public async Task DoExportAsync(Stream outStream, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
         {
-            var backupObject = await GetBackupObjectAsync(progressCallback);
-            backupObject.SerializeJson(backupStream);
-        }
-
-        public async Task DoImportAsync(Stream backupStream, Action<ExportImportProgressInfo> progressCallback)
-        {
-            var backupObject = backupStream.DeserializeJson<BackupObject>();
-            var progressInfo = new ExportImportProgressInfo();
-
-            progressInfo.Description = String.Format("{0} tagged items importing...", backupObject.TaggedItems.Count);
+            cancellationToken.ThrowIfCancellationRequested();
+            var progressInfo = new ExportImportProgressInfo {Description = "loading data..."};
             progressCallback(progressInfo);
 
-            await _taggedItemService.SaveChangesAsync(backupObject.TaggedItems.ToArray());
-        }
-
-        private async Task<BackupObject> GetBackupObjectAsync(Action<ExportImportProgressInfo> progressCallback)
-        {
-            var taggedItems = await _taggedItemSearchService.SearchTaggedItemsAsync(new TaggedItemSearchCriteria { Take = int.MaxValue });
-            var progressInfo = new ExportImportProgressInfo { Description = String.Format("{0} tagged items loading...", taggedItems.TotalCount) };
-            progressCallback(progressInfo);
-            var retVal = new BackupObject
+            using (var sw = new StreamWriter(outStream, System.Text.Encoding.UTF8))
+            using (var writer = new JsonTextWriter(sw))
             {
-                TaggedItems = taggedItems.Results
-            };
+                await writer.WriteStartObjectAsync();
 
-            return retVal;
+                var taggedItems = await _taggedItemSearchService.SearchTaggedItemsAsync(new TaggedItemSearchCriteria {Take = int.MaxValue});
+                progressInfo.Description = $"{taggedItems.TotalCount} tagged items loading...";
+                progressCallback(progressInfo);
+
+                await writer.WritePropertyNameAsync("TaggedItemsTotalCount");
+                await writer.WriteValueAsync(taggedItems.TotalCount);
+
+                await writer.WritePropertyNameAsync("TaggedItems");
+                await writer.WriteStartArrayAsync();
+
+                foreach (var taggedItem in taggedItems.Results)
+                {
+                    _serializer.Serialize(writer, taggedItem);
+                }
+
+                await writer.FlushAsync();
+                progressInfo.Description = $"{taggedItems.TotalCount} tagged items exported";
+                progressCallback(progressInfo);
+
+                await writer.WriteEndArrayAsync();
+                await writer.WriteEndObjectAsync();
+                await writer.FlushAsync();
+            }
+        }
+
+        public async Task DoImportAsync(Stream inputStream, Action<ExportImportProgressInfo> progressCallback, ICancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var progressInfo = new ExportImportProgressInfo();
+            progressInfo.Description = "tagged items importing...";
+            progressCallback(progressInfo);
+
+            using (var streamReader = new StreamReader(inputStream))
+            using (var reader = new JsonTextReader(streamReader))
+            {
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.PropertyName)
+                    {
+                        if (reader.Value.ToString() == "TaggedItems")
+                        {
+                            await reader.DeserializeJsonArrayWithPagingAsync<TaggedItem>(_serializer, _batchSize,
+                                async items => { await _taggedItemService.SaveChangesAsync(items.ToArray()); }, processedCount =>
+                                {
+                                    progressInfo.Description = $"{processedCount} Tagged items have been imported";
+                                    progressCallback(progressInfo);
+                                }, cancellationToken);
+                        }
+                    }
+                }
+            }
         }
     }
 }
