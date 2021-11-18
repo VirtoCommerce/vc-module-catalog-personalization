3b25651c59651596bb12b77893d40eee3c5ca653
Andrei Kolchanov <akak1977@gmail.com>
2021-11-18 21:23:36 +0500

    feat: Bump target framework and basic dependencies up to Net6

--- src/VirtoCommerce.CatalogPersonalizationModule.Data/Repositories/PersonalizationDbContext.cs
+++ src/VirtoCommerce.CatalogPersonalizationModule.Data/Repositories/PersonalizationDbContext.cs
@@ -21,7 +21,7 @@ namespace VirtoCommerce.CatalogPersonalizationModule.Data.Repositories
             modelBuilder.Entity<TaggedItemEntity>().Property(x => x.Id).HasMaxLength(128).ValueGeneratedOnAdd();
             modelBuilder.Entity<TaggedItemEntity>().HasIndex(x => new { x.ObjectId, x.ObjectType })
                 .IsUnique(true)
-                .HasName("IX_ObjectId_ObjectType");
+                .HasDatabaseName("IX_ObjectId_ObjectType");
 
             modelBuilder.Entity<TagEntity>().ToTable("Tag").HasKey(x => x.Id);
             modelBuilder.Entity<TagEntity>().Property(x => x.Id).HasMaxLength(128).ValueGeneratedOnAdd();
@@ -34,7 +34,7 @@ namespace VirtoCommerce.CatalogPersonalizationModule.Data.Repositories
                 .OnDelete(DeleteBehavior.Cascade);
 
             // Adding index to Outline column for faster search
-            modelBuilder.Entity<TaggedItemOutlineEntity>().HasIndex(x => x.Outline).IsUnique(false).HasName("IX_Outline");
+            modelBuilder.Entity<TaggedItemOutlineEntity>().HasIndex(x => x.Outline).IsUnique(false).HasDatabaseName("IX_Outline");
 
             base.OnModelCreating(modelBuilder);
         }
